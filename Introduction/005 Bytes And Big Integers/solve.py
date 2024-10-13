47b33f4529360a40102df657dbf1ce75b2debb35
databunks <sahaq2001@gmail.com>
2024-10-13 10:29:20 +0100

    bytes, big integers

---
+++ Introduction/005 Bytes And Big Integers/solve.py
@@ -0,0 +1,10 @@
+from Crypto.Util.number import *
+
+string = 11515195063862318899931685488813747395775516287289682636499965282714637259206269 
+
+string_bytes = long_to_bytes(string)
+
+print(string_bytes)
+
+
+
